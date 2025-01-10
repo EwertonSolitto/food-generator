7635d2cc2e5cc9364c01612ef576bff5dadb2eae
Ewerton <ewerton2004@yahoo.com.br>
2025-01-09 21:47:15 -0300

    feat: create function to generate another food

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,26 +3,32 @@ import { Image, Text, TouchableHighlight, View } from 'react-native';
 
 import { styles } from './styles';
 import getImage from '../../api/getImage';
+import { generateAnotherFood } from './actions';
 
 export default function Home() {
   const [foodImage, setFoodImage] = useState('')
+  const [cooldown, setCooldown] = useState(false)
   
   useEffect(() => {
-    (async () => {
-      const {image} = await getImage()
-      if(image) {
-        setFoodImage(image)
-      }
-    })()
+    (async () => await generateAnotherFood(setFoodImage, foodImage))()
   }, [])
 
+  async function generateFood() {
+    setCooldown(true)
+    await generateAnotherFood(setFoodImage, foodImage)
+    setTimeout(() => setCooldown(false), 2000)
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Food Generator</Text>
       <Image style={styles.image} source={{uri: foodImage}} resizeMode='cover'/>
       
-      <TouchableHighlight style={styles.buttonContainer}>
+      <TouchableHighlight 
+        style={styles.buttonContainer} 
+        onPressOut={generateFood} 
+        disabled={cooldown}
+      >
         <Text style={styles.buttonText}>Generate food</Text>
       </TouchableHighlight>
 
