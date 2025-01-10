04e4572a6dec0d698044127cd952f5cb6927d16f
Ewerton <ewerton2004@yahoo.com.br>
2025-01-09 21:29:15 -0300

    feat: create interface and create function to load image

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, Text, TouchableHighlight, View } from 'react-native';
 
 import { styles } from './styles';
+import getImage from '../../api/getImage';
 
 export default function Home() {
+  const [foodImage, setFoodImage] = useState('')
+  
+  useEffect(() => {
+    (async () => {
+      const {image} = await getImage()
+      if(image) {
+        setFoodImage(image)
+      }
+    })()
+  }, [])
+
+
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Food Generator</Text>
+      <Image style={styles.image} source={{uri: foodImage}} resizeMode='cover'/>
       
+      <TouchableHighlight style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Generate food</Text>
+      </TouchableHighlight>
+
     </View>
   );
 }
