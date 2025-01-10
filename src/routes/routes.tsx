f67dd1210001265ce1ba4c31e954da3709d6453d
Ewerton <ewerton2004@yahoo.com.br>
2025-01-10 10:10:04 -0300

    feat: add more 3 items to bottom tab and style it

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,23 +1,72 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Home from '../screens/home';
+import Burger from '../screens/burger';
+import Pasta from '../screens/pasta';
+import Pizza from '../screens/pizza';
+
+import colors from '../styles/colors';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export default function Routes() {
-
-  
   return (
     <Navigator screenOptions={{
-      headerShown: false
+      headerShown: false,
+      headerBackgroundContainerStyle: {
+        backgroundColor: colors.secondary
+      },
+      tabBarStyle: {
+        height: 64,
+        borderTopWidth: 0,
+      },
+      tabBarInactiveTintColor: "#c4c4c4",
+      tabBarInactiveBackgroundColor: colors.secondary,
+      tabBarActiveBackgroundColor: colors.primary,
+      tabBarShowLabel: false,
+      tabBarIconStyle: {
+        height: 48,
+        width: 48
+      },
     }}>
       <Screen 
-        name="Foods" 
+        name="Food" 
         component={Home}
         options={{
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="food" size={size} color={color}/>
-          )
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="food" size={40} color={color}/>
+          ),
+          tabBarActiveTintColor: "#0B77D5"
+        }} 
+      />
+      <Screen 
+        name="Burger" 
+        component={Burger}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="hamburger" size={40} color={color}/>
+          ),
+          tabBarActiveTintColor: "#44AA0E"
+        }} 
+      />
+      <Screen 
+        name="Pasta" 
+        component={Pasta}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="pasta" size={40} color={color}/>
+          ),
+          tabBarActiveTintColor: "#E11414"
+        }} 
+      />
+      <Screen 
+        name="Pizza" 
+        component={Pizza}
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="pizza" size={40} color={color}/>
+          ),
+          tabBarActiveTintColor: "#ECB309"
         }} 
       />
     </Navigator>
